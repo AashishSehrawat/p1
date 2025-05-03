221d1af36f00b253b6d344f608a5b6554f52f558
AashishSehrawat <aashishsehrawat98174@gmail.com>
2025-05-03 15:20:11 +0530

    completed till footer except some link

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Footer = () => {
+  const today = new Date();
+  let currYear = today.getFullYear();
   return (
-    <div>
-      footer
+    <div className="footer">
+      <div className="footerContent">
+        <div>
+          <h2>Getting Started</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia harum
+            rerum magnam obcaecati asperiores neque aut suscipit. Doloribus,
+            suscipit reiciendis!
+          </p>
+        </div>
+        <Link to="/" className="button">
+          Register Now
+        </Link>
+      </div>
+      <div className="footerLink">
+        <div className="footerLinkContent">
+          <div className="footerLinkImgContent">
+            <img src={logo} alt="Logo" />
+          </div>
+          <p>
+            CareerSathi EdTech Pvt. Ltd. <br />
+            Ashoka garden <br />
+            Bhopal, MP – 462023 <br />
+            India
+          </p>
+          <div className="footerLinkPandE">
+            <div className="footerLinkPhone">
+              <p>Phone Number</p>
+              <p>+91-8340783239</p>
+            </div>
+            <div className="footerLinkEmail">
+              <p>Email</p>
+              <p>officialcareersathi@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="footerDirectLink">
+          <div className="footerQuickLink">
+            <h3>Quick Link</h3>
+            <div className="footerAllQuick">
+              <Link className="link" to="/">Home</Link>
+              <Link className="link" to="/">Services</Link>
+              <Link className="link" to="/">About Us</Link>
+              <Link className="link" to="/">Contact Us</Link>
+            </div>
+          </div>
+          <div className="footerSocialLink">
+            <h3>Social</h3>
+            <div className="footerAllSocial">
+              <Link className="link" to="/">Instagram</Link>
+              <Link className="link" to="/">Linkdin</Link>
+              <Link className="link" to="/">Twitter</Link>
+            </div>
+          </div>
+          <div className="footerDataContent">
+            © {currYear} Career Sathi. All rights reserved.
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,62 +1,78 @@
 import React, { lazy, useRef } from "react";
 import mainImage from "../assets/mainSideImage.webp";
-import boostImg from '../assets/boostImg.webp';
+import boostImg from "../assets/boostImg.webp";
 import check from "../assets/check.svg";
 import ai from "../assets/serviceSvg/aiSupport.webp";
 import person from "../assets/testimonial/4.png";
 import aboutUs from "../assets/aboutUs.webp";
+import qr from "../assets/contact/contactQR.jpg";
+import email from "../assets/contact/email.svg";
+import phone from "../assets/contact/phone.svg";
+import office from "../assets/contact/office.svg";
+import twitter from "../assets/contact/tw.svg";
+import linkdin from "../assets/contact/link.svg";
+import insta from "../assets/contact/insta.svg";
 
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { Link } from "react-router-dom";
-import { TestimonialNextArrows, TestimonialPrevArrows } from "../components/TestimonialArrows.jsx";
+import {
+  TestimonialNextArrows,
+  TestimonialPrevArrows,
+} from "../components/TestimonialArrows.jsx";
 
 const Header = lazy(() => import("../components/Header.jsx"));
 const Footer = lazy(() => import("../components/Footer.jsx"));
 const ServiceCard = lazy(() => import("../components/ServiceCard.jsx"));
-const TestimonialCardCom = lazy(() => import("../components/TestimonialCardCom.jsx"));
+const TestimonialCardCom = lazy(() =>
+  import("../components/TestimonialCardCom.jsx")
+);
 
 const Home = () => {
-
   // useref hook scroll down
   const serviceRef = useRef(null);
   const contactRef = useRef(null);
   const aboutRef = useRef(null);
 
-
   // service card static data
   const firstServiceCard = {
     heading: "Smart Career Start",
-    img: {ai},
-    content: "AI-driven assessments to unlock the best career paths for students."
-  }
+    img: { ai },
+    content:
+      "AI-driven assessments to unlock the best career paths for students.",
+  };
   const secondServiceCard = {
     heading: "Future Fit",
-    img: {ai},
-    content: "Personalized career roadmaps based on personality, skills, and market trends."
-  }
+    img: { ai },
+    content:
+      "Personalized career roadmaps based on personality, skills, and market trends.",
+  };
   const thirdServiceCard = {
     heading: "Mentor Magic",
-    img: {ai},
-    content: "Real insights from top college alumni for informed career decisions."
-  }
+    img: { ai },
+    content:
+      "Real insights from top college alumni for informed career decisions.",
+  };
   const fourthServiceCard = {
     heading: "Pathfinder Pro",
-    img: {ai},
-    content: "Science-backed guidance to match students with their ideal careers."
-  }
+    img: { ai },
+    content:
+      "Science-backed guidance to match students with their ideal careers.",
+  };
   const fifthServiceCard = {
     heading: "Trend Track",
-    img: {ai},
-    content: "Career advice aligned with emerging job markets and future opportunities."
-  }
+    img: { ai },
+    content:
+      "Career advice aligned with emerging job markets and future opportunities.",
+  };
   const sixthServiceCard = {
     heading: "Unlock Potential",
-    img: {ai},
-    content: "Discover hidden strengths and ideal career fits with AI-powered analysis."
-  }
+    img: { ai },
+    content:
+      "Discover hidden strengths and ideal career fits with AI-powered analysis.",
+  };
 
   // testimonial data
   const testimonialData = [
@@ -64,19 +80,22 @@ const Home = () => {
       name: "Nittin",
       img: person,
       college: "IIT Delhi",
-      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus culpa dolorum eius ut? Id vel odit dolorem, quo aut voluptas beatae nostrum nemo! Veniam similique corrupti dolores nobis magnam iste sequi tempora ex aperiam labore."
+      content:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus culpa dolorum eius ut? Id vel odit dolorem, quo aut voluptas beatae nostrum nemo! Veniam similique corrupti dolores nobis magnam iste sequi tempora ex aperiam labore.",
     },
     {
       name: "ASH",
       img: person,
       college: "IIT Delhi",
-      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus culpa dolorum eius ut? Id vel odit dolorem, quo aut voluptas beatae nostrum nemo! Veniam similique corrupti dolores nobis magnam iste sequi tempora ex aperiam labore."
+      content:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus culpa dolorum eius ut? Id vel odit dolorem, quo aut voluptas beatae nostrum nemo! Veniam similique corrupti dolores nobis magnam iste sequi tempora ex aperiam labore.",
     },
     {
       name: "ankitn",
       img: person,
       college: "IIT Delhi",
-      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus culpa dolorum eius ut? Id vel odit dolorem, quo aut voluptas beatae nostrum nemo! Veniam similique corrupti dolores nobis magnam iste sequi tempora ex aperiam labore."
+      content:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus culpa dolorum eius ut? Id vel odit dolorem, quo aut voluptas beatae nostrum nemo! Veniam similique corrupti dolores nobis magnam iste sequi tempora ex aperiam labore.",
     },
   ];
 
@@ -87,21 +106,20 @@ const Home = () => {
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay: true,      
-    autoplaySpeed: 3000, 
-    arrows: true,        
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
     pauseOnHover: true,
-    nextArrow: <TestimonialNextArrows/>,
-    prevArrow: <TestimonialPrevArrows/>,  
-
+    nextArrow: <TestimonialNextArrows />,
+    prevArrow: <TestimonialPrevArrows />,
   };
 
   return (
     <div className="homeSection">
-      <Header  
-        aboutRef = {aboutRef}
-        serviceRef = {serviceRef}
-        contactRef = {contactRef}
+      <Header
+        aboutRef={aboutRef}
+        serviceRef={serviceRef}
+        contactRef={contactRef}
       />
 
       {/* main section */}
@@ -136,12 +154,26 @@ const Home = () => {
             <div className="boostContentBlock">
               <h2>Boost College Getting</h2>
               <div className="boostContentPointers">
-                <div><img src={check} alt="check mark" /><span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, ea facere nulla dignissimos ab eaque!</span></div>
-                <div><img src={check} alt="check mark" /><span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, ea facere nulla dignissimos ab eaque!</span></div>
+                <div>
+                  <img src={check} alt="check mark" />
+                  <span>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Porro, ea facere nulla dignissimos ab eaque!
+                  </span>
+                </div>
+                <div>
+                  <img src={check} alt="check mark" />
+                  <span>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Porro, ea facere nulla dignissimos ab eaque!
+                  </span>
+                </div>
               </div>
-              <Link to="" className="button">Register Now</Link>
+              <Link to="" className="button">
+                Register Now
+              </Link>
             </div>
-          </div>  
+          </div>
         </div>
       </div>
 
@@ -166,22 +198,19 @@ const Home = () => {
           <h2>Discover What Our Students Speak</h2>
           <div>
             <Slider {...settingsTest}>
-            {
-              testimonialData.map((item, index) => (
-                <TestimonialCardCom  
-                  key = {index}
-                  name = {item.name}
-                  img = {item.img}
-                  college = {item.college}
-                  content = {item.content}
+              {testimonialData.map((item, index) => (
+                <TestimonialCardCom
+                  key={index}
+                  name={item.name}
+                  img={item.img}
+                  college={item.college}
+                  content={item.content}
                 />
-              ))
-            }
+              ))}
             </Slider>
           </div>
         </div>
       </div>
-
 
       {/* about section */}
       <div ref={aboutRef} className="aboutSection">
@@ -189,13 +218,26 @@ const Home = () => {
           <div className="aboutBlocks">
             <div className="aboutContentBlock">
               <div>
-              <h2>About Us</h2>
-              <h4>About CareerSathi – Your Career Compass</h4>
-              <p>CareerSathi is revolutionizing career guidance for India’s students. We cut through the confusion with personalized, AI-powered advice combined with human expertise. Our mission is simple: help students in classes 8-12 discover their perfect career path with confidence.</p>
-              <p>We make career decisions easier with psychometric tests, expert mentorship, and up-to-date industry insights. Whether you're exploring STEM, arts, or unconventional careers, we provide the clarity you need to shape your future. CareerSathi isn’t just guidance – it’s your roadmap to success.</p>
-              <Link to="/aboutUs" className="button">Know more</Link>
+                <h2>About Us</h2>
+                <h4>About CareerSathi – Your Career Compass</h4>
+                <p>
+                  CareerSathi is revolutionizing career guidance for India’s
+                  students. We cut through the confusion with personalized,
+                  AI-powered advice combined with human expertise. Our mission
+                  is simple: help students in classes 8-12 discover their
+                  perfect career path with confidence.
+                </p>
+                <p>
+                  We make career decisions easier with psychometric tests,
+                  expert mentorship, and up-to-date industry insights. Whether
+                  you're exploring STEM, arts, or unconventional careers, we
+                  provide the clarity you need to shape your future. CareerSathi
+                  isn’t just guidance – it’s your roadmap to success.
+                </p>
+                <Link to="/aboutUs" className="button">
+                  Know more
+                </Link>
               </div>
-             
             </div>
             <div className="aboutImgBlock">
               <img src={aboutUs} alt="About Us" />
@@ -204,13 +246,45 @@ const Home = () => {
         </div>
       </div>
 
-      {/* conatct section */ }
+      {/* conatct section */}
       <div ref={contactRef} className="contactSection">
         <div className="container">
           <div className="contactBlocks">
-            <div className="contactHeadingBlock">Reach Us</div>
+            <div className="contactImgBlock">
+              <img src={qr} alt="Qr code" />
+              <Link to="" className="button">
+                Contact Us
+              </Link>
+            </div>
             <div className="contactContentBlock">
-              
+              <div>
+                <h2>Contact Us</h2>
+                <p>
+                  Email, call or scan the QR code to learn how Career Sathi can
+                  solve your problem.
+                </p>
+                <p>
+                  <img src={email} alt="email id" />
+                  officialcareersathi@gmail.com
+                </p>
+                <p>
+                  <img src={phone} alt="Phone number" /> +91-8340783239
+                </p>
+                <p>
+                  <img src={office} alt="office" />
+                  CareerSathi EdTech Pvt. Ltd. Ashoka garden Bhopal, MP – 462023
+                  India
+                </p>
+                <div className="contactFollowBlock">
+                  <h3>Follow Us</h3>
+                  <h4>"Join our community for career inspiration and updates!"</h4>
+                  <div>
+                    <Link><img src={insta} width="45px" alt="Instagram" /></Link>
+                    <Link><img src={linkdin} width="53px" alt="Linkdin" /></Link>
+                    <Link><img src={twitter} width="40px" alt="Twitter" /></Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
